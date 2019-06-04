5f31bdf4ea49e42e2d32a5c59bcf1ba6696adb7c
Kostas Aronis <k.aronis@dotbydot.gr>
2019-06-04 15:01:02 +0300

    removed method that was already here

--- src/Panacea/Implementations/HttpClient.cs
+++ src/Panacea/Implementations/HttpClient.cs
@@ -388,16 +388,5 @@ namespace Panacea.Implementations
         {
             return BuildUrl(path);
         }
-        public async Task<string> GetStringAsync(
-            string url,
-            List<KeyValuePair<string, string>> postData = null,
-            Dictionary<string, byte[]> files = null,
-            bool allowCache = true,
-            CancellationTokenSource cts = null)
-        {
-            url = await BuildUrl(url);
-            return await DownloadString(url, postData, files, allowCache, cts);
-        }
     }
-
 }
