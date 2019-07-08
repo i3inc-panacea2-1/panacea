141c217fc6e21d084a13f75ca0fb516f740a7e9e
Portno <j.remoundos@dotbydot.gr>
2019-07-08 13:51:42 +0300

    fixes

--- src/Panacea/App.xaml.cs
+++ src/Panacea/App.xaml.cs
@@ -42,80 +42,89 @@ namespace Panacea
 
         private async void Application_Startup(object sender, StartupEventArgs e)
         {
-            _splash = new Controls.SplashScreen();
-            _splash.Show();
-            if (Program.StartupArgs["noupdate"] != "1" && Program.StartupArgs["offline"] != "1")
+            try
             {
-                await StartUpdater();
-                return;
-            }
-            SetProgress("Getting configuration...");
+                _splash = new Controls.SplashScreen();
+                _splash.Show();
+                if (Program.StartupArgs["noupdate"] != "1" && Program.StartupArgs["offline"] != "1")
+                {
+                    await StartUpdater();
+                    return;
+                }
+                SetProgress("Getting configuration...");
 
-            var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var pluginsPath = Path.Combine(basePath, "Plugins");
-            if (!Directory.Exists(pluginsPath))
+                var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var pluginsPath = Path.Combine(basePath, "Plugins");
+                if (!Directory.Exists(pluginsPath))
+                {
+                    pluginsPath = Path.Combine(new DirectoryInfo(basePath).Parent.Parent.Parent.Parent.Parent.Parent.Parent.FullName, "Modules");
+                }
+
+                var kernel = new StandardKernel();
+                var serializer = new PanaceaSerializer();
+                kernel.Bind<ISerializer>().ToConstant(serializer);
+
+                _logger = new EventViewerLogger("Panacea");
+                kernel.Bind<ILogger>().ToConstant(_logger);
+                _logger.Info(this, "Hi!");
+                var identification = new TerminalIdentifier(serializer);
+                var putik = await identification.GetIdentifierAsync();
+
+                var settings = new PanaceaRegistrySettingsManager(serializer);
+
+                var cache = new SqLiteNetworkCache(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "cache"), "cache.db");
+                var httpClient = new HttpClient(
+                    new Uri((await settings.GetRegistrationInfo()).HospitalServer),
+                    0,
+                    cache,
+                    serializer,
+                    _logger);
+                var userService = new UserService(httpClient, _logger);
+                httpClient.AddMiddleware(new UserAuthenticationMiddleware(userService));
+                httpClient.AddMiddleware(new TerminalIdentificationMiddleware(putik));
+                var loader = new PluginLoader(kernel, _logger);
+                var webSocket = new WebSocketCommunicator(putik);
+                webSocket.Connect();
+                await userService.LoginFromFileAsync();
+                _core = new PanaceaServices(httpClient, userService, loader, _logger, webSocket, serializer);
+                kernel.Bind<PanaceaServices>().ToConstant(_core);
+                var dir = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+                var pluginsToLoad = (await GetPluginsAsync()).ToList();
+                var exclude = new List<string>();
+                if (Program.StartupArgs["exclude"] != null)
+                {
+                    exclude = Program.StartupArgs["exclude"].ToLower().Split(',').ToList();
+                }
+
+                if (Program.StartupArgs["include"] != null)
+                {
+                    var letters = Program.StartupArgs["include"].ToLower().Split(',');
+                    pluginsToLoad = pluginsToLoad.Concat(letters).ToList();
+                }
+
+
+                //if (Debugger.IsAttached)
+                //{
+                //    new DevConsole(logger).Show();
+                //}
+                //Console.WriteLine("-------------");
+                var watch = new Stopwatch();
+                watch.Start();
+                SetProgress("Loading plugins...");
+                await loader.LoadPlugins(pluginsPath, pluginsToLoad, exclude, e.Args);
+
+                watch.Stop();
+                _logger.Info(this, $"Time to load plugins:  {watch.ElapsedMilliseconds.ToString()}ms");
+                _splash.Close();
+                _splash = null;
+            }
+            catch (Exception ex)
             {
-                pluginsPath = Path.Combine(new DirectoryInfo(basePath).Parent.Parent.Parent.Parent.Parent.Parent.Parent.FullName, "Modules");
+                SetProgress(ex.Message);
+                await Task.Delay(5000);
+                ShutDownSafe();
             }
-
-            var kernel = new StandardKernel();
-            var serializer = new PanaceaSerializer();
-            kernel.Bind<ISerializer>().ToConstant(serializer);
-
-            _logger = new EventViewerLogger("Panacea");
-            kernel.Bind<ILogger>().ToConstant(_logger);
-            _logger.Info(this, "Hi!");
-            var identification = new TerminalIdentifier(serializer);
-            var putik = await identification.GetIdentifierAsync();
-
-            var settings = new PanaceaRegistrySettingsManager(serializer);
-
-            var cache = new SqLiteNetworkCache(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "cache"), "cache.db");
-            var httpClient = new HttpClient(
-                new Uri((await settings.GetRegistrationInfo()).HospitalServer),
-                0,
-                cache,
-                serializer,
-                _logger);
-            var userService = new UserService(httpClient, _logger);
-            httpClient.AddMiddleware(new UserAuthenticationMiddleware(userService));
-            httpClient.AddMiddleware(new TerminalIdentificationMiddleware(putik));
-            var loader = new PluginLoader(kernel, _logger);
-            var webSocket = new WebSocketCommunicator(putik);
-            webSocket.Connect();
-            await userService.LoginFromFileAsync();
-            _core = new PanaceaServices(httpClient, userService, loader, _logger, webSocket, serializer);
-            kernel.Bind<PanaceaServices>().ToConstant(_core);
-            var dir = new DirectoryInfo(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-
-            var pluginsToLoad = (await GetPluginsAsync()).ToList();
-            var exclude = new List<string>();
-            if (Program.StartupArgs["exclude"] != null)
-            {
-                exclude = Program.StartupArgs["exclude"].ToLower().Split(',').ToList();
-            }
-
-            if (Program.StartupArgs["include"] != null)
-            {
-                var letters = Program.StartupArgs["include"].ToLower().Split(',');
-                pluginsToLoad = pluginsToLoad.Concat(letters).ToList();
-            }
-
-
-            //if (Debugger.IsAttached)
-            //{
-            //    new DevConsole(logger).Show();
-            //}
-            //Console.WriteLine("-------------");
-            var watch = new Stopwatch();
-            watch.Start();
-            SetProgress("Loading plugins...");
-            await loader.LoadPlugins(pluginsPath, pluginsToLoad, exclude, e.Args);
-
-            watch.Stop();
-            _logger.Info(this, $"Time to load plugins:  {watch.ElapsedMilliseconds.ToString()}ms");
-            _splash.Close();
-            _splash = null;
         }
 
         async Task<IEnumerable<string>> GetPluginsAsync()
@@ -192,6 +201,7 @@ namespace Panacea
         {
 
             LogException(e.Exception);
+            ShdutDownSafe();
         }
 
         private void UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -207,12 +217,23 @@ namespace Panacea
                 ex = new NotSupportedException("Unhandled exception doesn't derive from System.Exception: " + e.ExceptionObject.ToString());
                 LogException(ex);
             }
+            ShdutDownSafe();
         }
 
         private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             e.Handled = true;
             LogException(e.Exception);
+            ShdutDownSafe();
+        }
+
+        void ShdutDownSafe()
+        {
+            if (Current?.Dispatcher != null)
+            {
+                Current.Dispatcher.Invoke(() => Shutdown());
+            }
+            else Shutdown();
         }
 
         void LogException(Exception ex)
